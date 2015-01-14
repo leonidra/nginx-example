c31a9680dd4e7a2af997304a2dd4cdea5743c7a3
Leonid Rashkovsky <leonid.r@fiverr.com>
2015-01-14 09:52:53 +0200

    Updated names in kitchen and recipe to install different nginx version

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,10 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-include_recipe "nginx"
-
+node.default['nginx']['source']['version'] = "1.6.2"
+node.default['nginx']['source']['url'] = 'http://nginx.org/download/nginx-1.6.2.tar.gz'
+node.default['nginx']['source']['checksum'] = 'b5608c2959d3e7ad09b20fc8f9e5bd4bc87b3bc8ba5936a513c04ed8f1391a18' 
 node.default['nginx']['default_site_enabled'] = false
 node.default['nginx']['default_root'] = '/var/www/example'
+
+include_recipe "nginx::source"
 
 template "#{node['nginx']['dir']}/sites-available/example" do
   source 'example-site.erb'
